e611fc8185da3d9bcf1d8eb7dd38e66fd181dc38
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-01-03 16:06:29 -0800

    Refactor base component

--- src/internal/base.js
+++ src/internal/base.js
@@ -1,7 +1,17 @@
 import React, { useRef, useEffect } from 'react'
 import micron from 'webkul-micron'
 
-const Base = ({ className = 'micro', children, type = 'custom', timing = 'ease-in', duration = 0.45, custom, styles: initialStyles }) => {
+const Base = ({
+  children,
+  event = 'click',
+  timing = 'ease-in',
+  duration = 0.45,
+  custom,
+  // Internal
+  className = 'micro',
+  type = 'custom',
+  styles: initialStyles
+}) => {
   const ref = useRef()
   useEffect(() => {
     const styles = !Array.isArray(initialStyles) ? [initialStyles] : initialStyles
@@ -10,10 +20,11 @@ const Base = ({ className = 'micro', children, type = 'custom', timing = 'ease-i
       styles.map.unuse(style => style.unuse())
     }
   }, [initialStyles])
-  const animation = typeof custom !== 'undefined' ? custom : (doc) => {
-    doc.getEle('.micro').interaction(type).duration(duration.toString()).timing(timing)
+  const animation = typeof custom !== 'undefined' ? custom : (el) => {
+    el.interaction(type).duration(duration.toString()).timing(timing)
   }
   const onClick = e => {
+    micron.getEle('.micro')
     animation(micron)
   }
   return <div className={className} ref={ref} onClick={onClick}>{children}</div>
